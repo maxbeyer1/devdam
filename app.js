6b186f80684473a3de87bbb2740d40d1f62a1be5
Max Beyer <max.beyer09@gmail.com>
2025-03-17 16:24:27 -0500

    feat: add asset management endpoints to main app routes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //
-// Express js (and node.js) web service that interacts with 
-// AWS S3 and RDS to provide clients data for building a 
+// Express js (and node.js) web service that interacts with
+// AWS S3 and RDS to provide clients data for building a
 // simple photo application for photo storage and viewing.
 //
 // Authors:
@@ -9,11 +9,11 @@
 //  Northwestern University
 //
 // References:
-// Node.js: 
+// Node.js:
 //   https://nodejs.org/
-// Express: 
+// Express:
 //   https://expressjs.com/
-// MySQL: 
+// MySQL:
 //   https://expressjs.com/en/guide/database-integration.html#mysql
 //   https://github.com/mysqljs/mysql
 // AWS SDK with JS:
@@ -23,13 +23,20 @@
 //   https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/javascript_s3_code_examples.html
 //
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const config = require('./config.js');
+const config = require("./config.js");
 
-const photoapp_db = require('./photoapp_db.js')
-const { HeadBucketCommand, ListObjectsV2Command } = require('@aws-sdk/client-s3');
-const { photoapp_s3, s3_bucket_name, s3_region_name } = require('./photoapp_s3.js');
+const photoapp_db = require("./photoapp_db.js");
+const {
+  HeadBucketCommand,
+  ListObjectsV2Command,
+} = require("@aws-sdk/client-s3");
+const {
+  photoapp_s3,
+  s3_bucket_name,
+  s3_region_name,
+} = require("./photoapp_s3.js");
 
 // support larger image uploads/downloads:
 app.use(express.json({ strict: false, limit: "50mb" }));
@@ -41,7 +48,7 @@ var startTime;
 //
 app.listen(config.service_port, () => {
   startTime = Date.now();
-  console.log('**Web service running, listening on port', config.service_port);
+  console.log("**Web service running, listening on port", config.service_port);
   //
   // Configure AWS to use our config file:
   //
@@ -51,19 +58,18 @@ app.listen(config.service_port, () => {
 //
 // request for default page /
 //
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   try {
     console.log("**Call to /...");
-    
+
     let uptime = Math.round((Date.now() - startTime) / 1000);
 
     res.json({
-      "status": "running",
+      status: "running",
       "uptime-in-secs": uptime,
-      "dbConnection": photoapp_db.state
+      dbConnection: photoapp_db.state,
     });
-  }
-  catch(err) {
+  } catch (err) {
     console.log("**Error in /");
     console.log(err.message);
 
@@ -76,40 +82,43 @@ app.get('/', (req, res) => {
 //
 
 // System operations
-let stats = require('./api_stats.js');
-let bucket = require('./api_bucket.js');
+let stats = require("./api_stats.js");
+let bucket = require("./api_bucket.js");
 
 // User management
-let users = require('./api_users.js');
-let user = require('./api_user.js');
+let users = require("./api_users.js");
+let user = require("./api_user.js");
 
 // Client management
-let clients = require('./api_clients.js');
+let clients = require("./api_clients.js");
 
 // Project management
-let projects = require('./api_projects.js');
+let projects = require("./api_projects.js");
 
 // Asset management
-let assets = require('./api_assets.js');
-let download = require('./api_image_get.js');
-let upload = require('./api_image_post.js');
+let assets = require("./api_assets.js");
+let asset = require("./api_asset.js");
+let asset_post = require("./api_asset_post.js");
 
-app.get('/stats', stats.get_stats);  
-app.get('/bucket', bucket.get_bucket);  
+app.get("/stats", stats.get_stats);
+app.get("/bucket", bucket.get_bucket);
 
-app.get('/users', users.get_users);  
-app.put('/user', user.put_user);
+app.get("/users", users.get_users);
+app.put("/user", user.put_user);
 
-app.get('/clients', clients.get_clients);
-app.get('/client/:clientid', clients.get_client);
-app.put('/client', clients.put_client);
-app.delete('/client/:clientid', clients.delete_client);
+app.get("/clients", clients.get_clients);
+app.get("/client/:clientid", clients.get_client);
+app.put("/client", clients.put_client);
+app.delete("/client/:clientid", clients.delete_client);
 
-app.get('/projects', projects.get_projects);
-app.get('/project/:projectid', projects.get_project);
-app.put('/project', projects.put_project);
-app.delete('/project/:projectid', projects.delete_project);
+app.get("/projects", projects.get_projects);
+app.get("/project/:projectid", projects.get_project);
+app.put("/project", projects.put_project);
+app.delete("/project/:projectid", projects.delete_project);
 
-app.get('/assets', assets.get_assets);  
-app.get('/image/:assetid', download.get_image);
-app.post('/image/:userid', upload.post_image);
+app.get("/assets", assets.get_assets);
+app.get("/asset/:assetid", asset.get_asset);
+app.put("/asset/:assetid", asset.put_asset);
+app.delete("/asset/:assetid", asset.delete_asset);
+app.get("/asset/:assetid/download", asset.download_asset);
+app.post("/asset/:projectid", asset_post.post_asset);
